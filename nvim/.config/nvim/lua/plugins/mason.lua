be5bbbcf21d61328bded79aa01fe4bbcd6e87824
Richard <richardadebowale.oye@gmail.com>
2026-05-16 21:24:26 -0400

    Configure Mason tooling

---
+++ nvim/.config/nvim/lua/plugins/mason.lua
@@ -0,0 +1,79 @@
+return {
+    {
+        "mason-org/mason.nvim",
+        cmd = "Mason",
+        opts = {
+            ui = {
+                icons = {
+                    package_installed = "✓",
+                    package_pending = "➜",
+                    package_uninstalled = "✗",
+                },
+            },
+        },
+    },
+
+    {
+        "mason-org/mason-lspconfig.nvim",
+        dependencies = { "mason-org/mason.nvim" },
+        opts = {
+            automatic_enable = true,
+        },
+    },
+
+    {
+        "WhoIsSethDaniel/mason-tool-installer.nvim",
+        dependencies = { "mason-org/mason.nvim" },
+        opts = {
+            ensure_installed = {
+                -- LSP
+                "pyright",
+                "lua-language-server",
+                "vtsls",
+                "clangd",
+                "html-lsp",
+                "css-lsp",
+                "json-lsp",
+                "yaml-language-server",
+
+                -- formatters
+                "black",
+                "stylua",
+                "prettier",
+                "biome",
+                "clang-format",
+                "shfmt",
+                "taplo",
+                "sqlfluff",
+
+                -- linters
+                "ruff",
+                "eslint_d",
+                "shellcheck",
+
+                -- DAP
+                "debugpy",
+                "codelldb",
+                "js-debug-adapter",
+            },
+            run_on_start = false,
+        },
+    },
+
+    {
+        "jay-babu/mason-nvim-dap.nvim",
+        dependencies = {
+            "mason-org/mason.nvim",
+            "mfussenegger/nvim-dap",
+        },
+        opts = {
+            automatic_installation = false,
+            ensure_installed = {
+                "python",
+                "codelldb",
+                "js",
+            },
+            handlers = nil,
+        },
+    },
+}
