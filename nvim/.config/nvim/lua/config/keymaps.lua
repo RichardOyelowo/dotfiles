546285a0002bc9e0d09b743406ca65f3d7ff68b8
Richard <richardadebowale.oye@gmail.com>
2026-05-16 21:24:26 -0400

    Add Neovim keymaps

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,66 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+local map = vim.keymap.set
+local opts = { noremap = true, silent = true }
+
+-- Basic keymaps
+map("i", "jj", "<ESC>")
+map({ "n", "v" }, "<leader>q", "<cmd>q!<CR>", { desc = "Quit" })
+map("t", "<Esc><Esc>", "<C-\\><C-n>", { desc = "Exit terminal" })
+map("n", "<Esc>", "<CMD>nohlsearch<CR>", { desc = "Clear search" })
+
+-- FIle & window keymaps
+map({ "n", "v" }, "<leader>w", "<cmd>w<CR>", { desc = "Save file" })
+map("n", "<leader>c", "<cmd>%y+<CR>", { desc = "Copy file contents" })
+map({ "n", "v" }, "<leader>h", "<cmd>split<CR>", { desc = "h split" })
+map({ "n", "v" }, "<leader>v", "<cmd>vsplit<CR>", { desc = "v split" })
+map("n", "<leader>n", "<cmd>set nu!<CR>", { desc = "toggle line number" })
+map("n", "<leader>rn", "<cmd>set rnu!<CR>", { desc = "toggle relative number" })
+
+-- Tab, shift-Tab: indent/unindent selection or current line
+map("v", "<Tab>", ">gv", opts)
+map("v", "<S-Tab>", "<gv", opts)
+map("n", "<Tab>", ">>", opts)
+map("n", "<S-Tab>", "<<", opts)
+
+-- Markdown configuration
+map("n", "<leader>mb", "<cmd>RenderMarkdown buf_toggle<CR>", { desc = "Toggle Markdown (buffer)" })
+map("n", "<leader>mr", "<cmd>RenderMarkdown toggle<CR>", { desc = "Toggle Markdown Render" })
+map("n", "<leader>mp", "<cmd>MarkdownPreviewToggle<CR>", { desc = "Toggle Markdown Preview" })
+
+-- whichkey
+map("n", "<leader>wK", "<cmd>WhichKey <CR>", { desc = "whichkey all keymaps" })
+map("n", "<leader>wk", function()
+    vim.cmd("WhichKey " .. vim.fn.input("WhichKey: "))
+end, { desc = "whichkey query lookup" })
+
+-- format file content
+map({ "n", "x" }, "<leader>fm", function()
+    require("conform").format({ lsp_fallback = true })
+end, { desc = "general format file" })
+
+-- global lsp mappings
+map("n", "<leader>ds", vim.diagnostic.setloclist, { desc = "LSP diagnostic loclist" })
+
+-- todo-comments
+map("n", "]t", function()
+    require("todo-comments").jump_next()
+end, { desc = "Next Todo Comment" })
+
+map("n", "[t", function()
+    require("todo-comments").jump_prev()
+end, { desc = "Previous Todo Comment" })
+
+map("n", "<leader>xt", "<cmd>Trouble todo toggle<CR>", { desc = "Todo (Trouble)" })
+map("n", "<leader>xT", "<cmd>Trouble todo toggle filter = {tag = {TODO,FIX,FIXME}}<CR>", {
+    desc = "Todo/Fix/Fixme (Trouble)",
+})
+
+map("n", "<leader>st", function()
+    Snacks.picker.todo_comments()
+end, { desc = "Todo" })
+
+map("n", "<leader>sT", function()
+    Snacks.picker.todo_comments({ keywords = { "TODO", "FIX", "FIXME" } })
+end, { desc = "Todo/Fix/Fixme" })
