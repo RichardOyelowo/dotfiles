8e4e9ca74855695cb96ae61e94eda25586e081fa
Richard <richardadebowale.oye@gmail.com>
2026-05-16 21:30:52 -0400

    Add debug keymaps

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -43,6 +43,39 @@ end, { desc = "general format file" })
 -- global lsp mappings
 map("n", "<leader>ds", vim.diagnostic.setloclist, { desc = "LSP diagnostic loclist" })
 
+-- debugging
+map("n", "<leader>db", function()
+    require("dap").toggle_breakpoint()
+end, { desc = "Toggle Breakpoint" })
+
+map("n", "<leader>dB", function()
+    require("dap").set_breakpoint(vim.fn.input("Breakpoint condition: "))
+end, { desc = "Conditional Breakpoint" })
+
+map("n", "<leader>dc", function()
+    require("dap").continue()
+end, { desc = "Debug Continue" })
+
+map("n", "<leader>di", function()
+    require("dap").step_into()
+end, { desc = "Step Into" })
+
+map("n", "<leader>do", function()
+    require("dap").step_over()
+end, { desc = "Step Over" })
+
+map("n", "<leader>dO", function()
+    require("dap").step_out()
+end, { desc = "Step Out" })
+
+map("n", "<leader>dr", function()
+    require("dap").repl.toggle()
+end, { desc = "Debug REPL" })
+
+map("n", "<leader>dt", function()
+    require("dap").terminate()
+end, { desc = "Debug Terminate" })
+
 -- todo-comments
 map("n", "]t", function()
     require("todo-comments").jump_next()
