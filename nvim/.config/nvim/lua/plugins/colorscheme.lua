52a70bacee973b03c2817725ada383eac0e9db8c
Richard <richardadebowale.oye@gmail.com>
2026-05-16 21:24:26 -0400

    Restore dark themes

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,11 +1,22 @@
 return {
-  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
-
-  -- Configure LazyVim to load gruvbox
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "catppuccin",
+    { "scottmckendry/cyberdream.nvim", name = "cyberdream" },
+    { "olimorris/onedarkpro.nvim", name = "onedarkpro" },
+    { "folke/tokyonight.nvim", name = "tokyonight" },
+    { "sainnhe/sonokai", name = "sonokai" },
+    { "catppuccin/nvim", name = "catppuccin" },
+    {
+        "rose-pine/neovim",
+        name = "rose-pine",
+        config = function()
+            vim.cmd("colorscheme rose-pine")
+        end,
     },
-  },
+
+    {
+        "LazyVim/LazyVim",
+        opts = {
+            colorscheme = "rose-pine",
+            transparent_background = true,
+        },
+    },
 }
