02d354180b95b74b616254201c4f55fc31930fd3
Richard <richardadebowale.oye@gmail.com>
2026-05-18 11:39:10 -0400

    Use direct WezTerm local return

--- wezterm/.config/wezterm/local.example.lua
+++ wezterm/.config/wezterm/local.example.lua
@@ -2,18 +2,19 @@
 -- Keep local.lua out of Git so absolute paths stay private.
 
 local img = "/absolute/path/to/background.jpg"
-local config = {}
 
 local file = io.open(img, "r")
 if file then
 	file:close()
 
-	config.window_background_image = img
-	config.window_background_image_hsb = {
-		brightness = 0.2,
-		saturation = 0.8,
-		hue = 0.5,
+	return {
+		window_background_image = img,
+		window_background_image_hsb = {
+			brightness = 0.2,
+			saturation = 0.8,
+			hue = 0.5,
+		},
 	}
 end
 
-return config
+return {}
