d1153f70c0e276d6fbc17d7098ce84d49d88682b
Richard <richardadebowale.oye@gmail.com>
2026-05-30 19:38:43 -0400

    updated theme to match 'catpuccin-mocha'

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -48,46 +48,17 @@ local config = {
 		DEV_TMUX = "1",
 	},
 
-	window_background_opacity = 0.88,
+	window_background_opacity = 0.95,
 	--kde_window_background_blur = false,
 	--macos_window_background_blur = 10,
+
+	color_scheme = "Catppuccin Mocha",
 
 	window_padding = {
 		left = 10,
 		right = 10,
 		top = 10,
 		bottom = 10,
-	},
-
-	colors = {
-		foreground = "#e0e0f0",
-		background = "#000000",
-
-		cursor_bg = "#e0e0f5",
-		cursor_fg = "#0a0c1f",
-		cursor_border = "#ff79c6",
-
-		ansi = {
-			"#0c0c17",
-			"#ff5c57",
-			"#5af78e",
-			"#f3f99d",
-			"#57c7ff",
-			"#ff6ac1",
-			"#9aedfe",
-			"#f1f1f0",
-		},
-
-		brights = {
-			"#1a1a2e",
-			"#ff5c57",
-			"#5af78e",
-			"#f3f99d",
-			"#57c7ff",
-			"#ff6ac1",
-			"#9aedfe",
-			"#f1f1f0",
-		},
 	},
 
 	check_for_updates = false,
